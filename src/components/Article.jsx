0fafe4bf7ae855f8069d0c3664b8019453eec84d
Nathaniel Butler <nathanielbutler@hotmail.com>
2024-09-16 15:30:17 +0100

    Complete task 7

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getArticle } from '../utils/api';
+import { getArticle, incrementArticleVotes } from '../utils/api';
 import { formatDateTimeString } from '../utils/helpers';
 import Loader from './Loader';
 import Comments from './Comments';
@@ -10,13 +10,29 @@ function Article() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [votes, setVotes] = useState();
+  const [error, setError] = useState('');
 
   useEffect(() => {
     getArticle(article_id).then((articleData) => {
       setArticle(articleData.article);
+      setVotes(articleData.article.votes);
       setIsLoading(false);
     });
   }, [article_id]);
+
+  const handleVoting = (vote) => {
+    setError('');
+    setVotes((currentVotes) => currentVotes + vote);
+    incrementArticleVotes(article_id, vote).catch((err) => {
+      setError('Voting unsuccessful!');
+      if (vote < 0) {
+        setVotes((currentVotes) => currentVotes + 1);
+      } else {
+        setVotes((currentVotes) => currentVotes - 1);
+      }
+    });
+  };
 
   if (isLoading) {
     return <Loader />;
@@ -32,8 +48,17 @@ function Article() {
         <img src={article.article_img_url} />
         <div className="votes">
           <p>
-            <strong>Votes:</strong> {article.votes}
+            <strong>Votes:</strong> {votes}
+            {error && <span className="error-msg">{error}</span>}
           </p>
+          <div className="voting-buttons">
+            <button type="button" onClick={() => handleVoting(1)}>
+              Up vote
+            </button>
+            <button type="button" onClick={() => handleVoting(-1)}>
+              Down vote
+            </button>
+          </div>
         </div>
         <p>{article.body}</p>
         <p className="author">By {article.author}</p>
